98e2de0055c836085dccc8f16943de69712b7f39
Juan Pablo Marin Buitrago <youtuzonhd@gmail.com>
2022-12-06 19:44:13 -0300

    "Corrigiendo el Método Mostrar"

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
-import Entidades.Nif;
 import Servicios.NifServicios;
 
 public class Main {
     public static void main(String[] args) {
 
         NifServicios ns = new NifServicios();
+
+        ns.menu();
 
     }
 }
--- src/Servicios/NifServicios.java
+++ src/Servicios/NifServicios.java
@@ -12,14 +12,16 @@ public class NifServicios {
 
     String[] vectorLetras = {"T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X",
             "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"};
+
     Nif n1 = new Nif();
 
     public Nif crearNif() {
 
-
+        System.out.println(n1.getDNI());
 
         System.out.println("Ingresa tu DNI (Debe ser de 8 dígitos)");
         n1.setDNI(sc.nextLong());
+
 
         while (n1.getDNI() < 99999 || n1.getDNI() > 99999999) {
             System.out.println("DNI incorrecto");
@@ -35,8 +37,43 @@ public class NifServicios {
 
     public void mostrar() {
 
-        String NIF = fmt.format("%08d", n1.getDNI()) + "-" + n1.getLetra();
-        System.out.println("Tu NIF es " + NIF);
+        String DNIString = String.valueOf(n1.getDNI());
+        String NIF;
+
+        if (n1.getLetra() == null) {
+            System.out.println("No has ingresado los datos para crear el NIF");
+        } else if (DNIString.length() < 8) {
+            NIF = "00" + DNIString + "-" + n1.getLetra();
+            System.out.println("Tu NIF es " + NIF);
+        } else {
+            NIF = DNIString + "-" + n1.getLetra();
+            System.out.println("Tu NIF es " + NIF);
+        }
+    }
+
+    public void menu() {
+
+        boolean salir = false;
+
+        do {
+
+            System.out.println("¿Qué deseas hacer?");
+            System.out.println("1. Crear NIF");
+            System.out.println("2. Mostrar NIF");
+            System.out.println("3. Salir");
+
+            int opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1 -> crearNif();
+                case 2 -> mostrar();
+                case 3 -> salir = true;
+                default -> System.out.println("Opción incorrecta");
+            }
+
+        } while (!salir);
+
+        System.out.println("Nos vemos pronto");
 
     }
 }
